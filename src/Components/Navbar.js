f1e3ea165393ecc148c20420751ef86a90e61ec4
Gustav72 <wolfgangkirstein7@gmail.com>
2023-03-07 21:53:12 -0500

    add boiler plate code

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -4,7 +4,7 @@ const Navbar = () => {
 
     return (
         <div>
-            <img/>
+            <a href="#home">Home</a>
             <a href="#home">Home</a>
             <a href="#products">Products</a>
         </div>
