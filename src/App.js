cda54e80061e131431f3a7a664ef09be12dfd349
Gustav72 <wolfgangkirstein7@gmail.com>
2023-03-16 22:32:03 -0400

    add abiility to add and delete cart items

--- src/App.js
+++ src/App.js
@@ -18,6 +18,30 @@ export default function App() {
     setCartItems([...cartItems, item]);
   }
 
+  function updateCartItem(item, action) {
+    setCartItems((prevItems) => {
+      const existingItemIndex = prevItems.findIndex(
+        (cartItem) => cartItem.product.name === item.name
+      );
+      if (existingItemIndex > -1) {
+        const updatedItems = [...prevItems];
+        if (action === "add") {
+          updatedItems[existingItemIndex].quantity += 1;
+        } else if (action === "delete") {
+          updatedItems[existingItemIndex].quantity -= 1;
+
+          if (updatedItems[existingItemIndex].quantity <= 0) {
+            updatedItems.splice(existingItemIndex, 1);
+          }
+        }
+        return updatedItems;
+      } else if (action === "add") {
+        return [...prevItems, { product: item, quantity: 1 }];
+      }
+      return prevItems;
+    });
+  }
+
   return (
     <>
       <div onClick={handleVisible}>
@@ -27,13 +51,16 @@ export default function App() {
             <Route path="/" element={<Home />} />
             <Route
               path="/products"
-              element={<Products onAddToCart={handleAddToCart} />}
+              element={
+                <Products onAddToCart={(item) => updateCartItem(item, "add")} />
+              }
             />
           </Routes>
         </div>
       </div>
       <Cart
         cartItems={cartItems}
+        onUpdateCartItems={updateCartItem}
         className={isVisible ? "cart-active" : "cart-inactive"}
       />
     </>
--- src/Components/Cart.js
+++ src/Components/Cart.js
@@ -1,14 +1,30 @@
-export default function Cart({ className, cartItems }) {
+export default function Cart({ className, cartItems, onUpdateCartItem }) {
+  const getTotalPrice = () => {
+    return cartItems.reduce(
+      (total, item) =>
+        total + parseFloat(item.product.price.slice(1)) * item.quantity,
+      0
+    );
+  };
+
   return (
     <div className={className}>
       <h1>Cart</h1>
       {cartItems.map((item, index) => (
         <div key={index} className="cart-item">
-          <h3>{item.name}</h3>
-          <img src={item.image} alt="puppy" />
-          <p>{item.price}</p>
+          <h3>{item.product.name}</h3>
+          <img src={item.product.image} alt="puppy" />
+          <p>{item.product.price}</p>
+          <p>Quantity: {item.quantity}</p>
+          <button onClick={() => onUpdateCartItem(item.product, "add")}>
+            Add
+          </button>
+          <button onClick={() => onUpdateCartItem(item.product, "delete")}>
+            Delete
+          </button>
         </div>
       ))}
+      <h2>Total Price: ${getTotalPrice().toFixed(2)}</h2>
     </div>
   );
 }
