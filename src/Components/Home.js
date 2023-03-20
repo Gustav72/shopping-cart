fe1b251260ad8f962440a9474590c50e937f698a
Gustav72 <wolfgangkirstein7@gmail.com>
2023-03-20 08:34:03 -0400

    Add graphic to home

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -2,18 +2,30 @@ import { Link } from "react-router-dom";
 
 export default function Home() {
   return (
-    <>
-      <div>
-        <img src="images/corgi.png" alt="forever puppy" />
+    <div>
+      <div className="home-container">
+        <div>
+          <h1>Puppies That Stay Puppies!</h1>
+          <h4>Once In A Lifetime Opportunity</h4>
+          <button>
+            <Link to="/products" className="link">
+              See Pups Now!
+            </Link>
+          </button>
+        </div>
+        <div>
+          <img
+            className="home-puppy"
+            src="images/shiba-inu.png"
+            alt="forever puppy"
+          />
+        </div>
       </div>
-      <div>
-        <h1>Buy Forever Pups Today!</h1>
-        <button>
-          <Link to="/products" className="link">
-            See Pups Now!
-          </Link>
-        </button>
-      </div>
-    </>
+      <img
+        className="home-image"
+        src="images/green-curve.png"
+        aria-label="none"
+      />
+    </div>
   );
 }
